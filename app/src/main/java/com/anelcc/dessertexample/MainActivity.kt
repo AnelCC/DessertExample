6497a5606c4a73502a6300b4fe8a2234cda3655c
Anel CC <anel.calvo.cervantes@gmail.com>
2020-02-16 20:02:52 -0500

    [EX00002] implement Lifecycle Methods

--- app/src/main/java/com/anelcc/dessertexample/MainActivity.kt
+++ app/src/main/java/com/anelcc/dessertexample/MainActivity.kt
@@ -134,8 +134,35 @@ class MainActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
+    /** Lifecycle Methods **/
+
     override fun onStart() {
         super.onStart()
-        Timber.i("onStart")
+        Timber.i("onStart Called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.i("onResume Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.i("onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Timber.i("onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.i("onDestroy Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Timber.i("onRestart Called")
     }
 }
